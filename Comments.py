2e468aa3a5b213ae1142dd8523f940ef013f0945
Nikhil Kamath <58089299+goober3@users.noreply.github.com>
2022-07-13 20:52:34 -0400

    added functionality to search youtube for queries. offloaded searches to another task so bot continues running

---
+++ Comments.py
@@ -0,0 +1,46 @@
+from googleapiclient.discovery import build
+import asyncio
+import time
+import config
+
+api_key = config.api_key
+
+
+async def youtube_get_comments(youtube, video_id, comments=None, token='', scrolls=5):
+    if comments is None:
+        comments = []
+
+    if not scrolls:
+        return comments
+
+    video_response = youtube.commentThreads().list(part='snippet',
+                                                   videoId=video_id,
+                                                   pageToken=token).execute()
+    for item in video_response['items']:
+        comment = item['snippet']['topLevelComment']
+        text = comment['snippet']['textDisplay']
+        comments.append(text)
+    if "nextPageToken" in video_response:
+        return await youtube_get_comments(youtube, video_id, comments, video_response['nextPageToken'], scrolls=scrolls-1)
+    else:
+        return comments
+
+
+async def youtube_search_keyword(youtube, query, max_results=10):
+    # sourcery skip: for-append-to-extend, list-comprehension
+    search_keyword = youtube.search().list(q=query, part="id, snippet",
+                                           maxResults=max_results).execute()
+
+    results = search_keyword.get("items", [])
+
+    titles = []
+    ids = []
+    for result in results:
+        if result['id']['kind'] == "youtube#video":
+            ids.append(result['id']['videoId'])
+            titles.append(result["snippet"]["title"])
+
+
+    return ids, titles
+
+
--- cogs/Youtube.py
+++ cogs/Youtube.py
@@ -1,9 +1,14 @@
+import asyncio
+import threading
 import discord
 from discord.ext import commands, tasks
 import os
 import random
 import pandas as pd
 from ytcomments import process_youtube_comments
+from googleapiclient.discovery import build
+import Comments
+import config
 
 
 '''
@@ -18,6 +23,8 @@ class Youtube(commands.Cog):
         self.bot = bot
         self.comments = pd.DataFrame()
         
+        
+        self.api_key = config.api_key
         self.res_folder = './resources'
         self.yt_folder = "yt"
         files = [f for f in os.listdir(os.path.join(self.res_folder, self.yt_folder)) if os.path.isfile(os.path.join(self.res_folder, self.yt_folder, f))]
@@ -34,10 +41,57 @@ class Youtube(commands.Cog):
     @commands.command()
     async def addyt(self, ctx, link):
         await ctx.send("collecting comments from youtube link")
-        temp, title = process_youtube_comments(link)
-        comments = pd.concat([comments, temp], ignore_index=True)
+        temp, title = process_youtube_comments(link)        
+        self.comments = pd.concat([self.comments, temp], ignore_index=True)
         await ctx.send(f"added {len(temp)} comments from {title}")
+        
+    '''use a youtube search to find videos with a given query and take comments from them
+    creates new event loop so that bot is still running during query'''
+    @commands.command()
+    async def searchyt(self, ctx, *, query):
+        await ctx.send(f"searching youtube for {query}") # send confirmation to discord channel
+        
+        asyncio.create_task(self.search_yt_helper(query, ctx))
+        
 
+    '''processes youtube comments in other event loop'''
+    async def search_yt_helper(self, query, confirmation_ctx):
+        # create call to youtube api
+        yt = build('youtube', 'v3', developerKey=self.api_key)
+        
+        # get video ids and titles from Comments package
+        video_ids, video_titles = await Comments.youtube_search_keyword(yt, query, max_results=5)
+        n = 0
+        new_comments = pd.DataFrame()
+        
+        # for each video, get comments from id and add them to new_comments dataframe
+        for v_id in video_ids:
+            c = await Comments.youtube_get_comments(yt, v_id, scrolls=3)
+            n += len(c)
+            temp = pd.DataFrame(c, columns=['comment'])
+            new_comments = pd.concat([new_comments, temp], ignore_index=True)
+            
+        print(f"added {n} comments from query")
 
-def setup(bot):
+        # create output message using the video titles returned by Comments package
+        output_message = f"added {len(new_comments)} comments from search '{query}'.\n"
+        output_message += "videos include: \n"
+        for title in video_titles:
+            output_message += title + "\n"
+        
+        # add new comments to Youtube cog's comments df
+        self.comments = pd.concat([new_comments, self.comments], ignore_index=True)
+        
+        # save new comments to pickle file
+        outdir = './resources/yt'
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+            
+        filename = "query." + query + ".pkl"
+        new_comments.to_pickle(os.path.join(outdir, filename))
+        
+        # send confirmation in original discord channel
+        await confirmation_ctx.send(output_message)
+        
+def setup(bot):  # sourcery skip: instance-method-first-arg-name
     bot.add_cog(Youtube(bot))
